641715ca3386efa5043df608f26d3b9f05cf9ce2
Abdelrahman Mohamed <abdelrahmanmhmd55@yahoo.com>
2026-04-20 01:11:34 +0200

    fix next-auth cookie handling for production

--- src/utils/getTokenFromCookies.ts
+++ src/utils/getTokenFromCookies.ts
@@ -4,7 +4,10 @@ import { cookies } from "next/headers";
 export async function getTokenFromCookies() {
   const myCookies = await cookies();
 
-  const tokenFromCookies = myCookies.get("next-auth.session-token")?.value;
+  const tokenFromCookies =
+    myCookies.get("next-auth.session-token")?.value ||
+    myCookies.get("__Secure-next-auth.session-token")?.value ||
+    myCookies.get("__Host-next-auth.session-token")?.value;
 
   const finalToken = await decode({
     token: tokenFromCookies,
